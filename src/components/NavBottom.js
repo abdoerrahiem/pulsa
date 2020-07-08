4f2ef94a4c33ebb0a7825312fb9d949de3a8708f
Abdur Rahim <abdoerrahiem@gmail.com>
2020-07-08 14:44:55 +0700

    edited navbar bottom order

--- src/components/NavBottom.js
+++ src/components/NavBottom.js
@@ -28,17 +28,17 @@ const NavBottom = ({ history }) => {
       </Link>
       <Link
         className='nav-item nav-link'
-        to='/search'
-        style={isActive(history, '/search')}
-      >
-        <i class='fas fa-list-ul fa-lg' />
-      </Link>
-      <Link
-        className='nav-item nav-link'
         to='/transfer'
         style={isActive(history, '/transfer')}
       >
         <i className='fas fa-university fa-lg' />
+      </Link>
+      <Link
+        className='nav-item nav-link'
+        to='/search'
+        style={isActive(history, '/search')}
+      >
+        <i class='fas fa-list-ul fa-lg' />
       </Link>
     </div>
   )
